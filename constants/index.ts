ab71c74b199013d86b6c52e3cab2ca2a4a11f243
Srijan Das <dassrijan16@gmail.com>
2024-07-20 21:39:02 +0530

    add routes and components

---
+++ constants/index.ts
@@ -0,0 +1,22 @@
+export const sidebarLinks = [
+    {
+        label: 'Upcoming',
+        route: '/upcoming',
+        imgUrl: '/icons/upcoming.svg'
+    },
+    {
+        label: 'Previous',
+        route: '/previous',
+        imgUrl: '/icons/previous.svg'
+    },
+    {
+        label: 'Recordings',
+        route: '/recordings',
+        imgUrl: '/icons/recordings.svg'
+    },
+    {
+        label: 'Personal Room',
+        route: '/personal-room',
+        imgUrl: '/icons/add-personal.svg'
+    }
+]
