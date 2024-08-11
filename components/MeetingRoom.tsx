494123dd9c30f39e8f9491a3b0d9f6334eb023ef
Srijan Das <dassrijan16@gmail.com>
2024-08-11 21:11:51 +0530

    Participant List Bug Fix

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,5 +1,8 @@
+"use client"
+
 import { cn } from '@/lib/utils'
-import { CallControls, CallingState, CallParticipantsList, CallStatsButton, PaginatedGridLayout, SpeakerLayout, useCallStateHooks } from '@stream-io/video-react-sdk'
+import { CallControls, CallingState, CallParticipantsList, CallStatsButton, 
+  PaginatedGridLayout, SpeakerLayout, useCallStateHooks } from '@stream-io/video-react-sdk'
 import React, { useState } from 'react'
 import { LayoutList, Users } from 'lucide-react'
 import {
@@ -41,14 +44,16 @@ const MeetingRoom = () => {
   return (
     <section className='relative h-screen w-full overflow-hidden
       pt-4 text-white'>
-        <div className="flex size-full max-w-[1000px] items-center">
-          <CallLayout />
-          {/* {CallLayout()} */}
-        </div>
-        <div className={cn('h-[calc(100vh-86px)] hidden ml-2', {
-          'show-block': showParticipants
-        })}>
-          <CallParticipantsList onClose={()=> setShowParticipants(false)} />
+        <div className='relative flex h-screen w-full items-center justify-center'>
+            <div className="flex size-full max-w-[1000px] items-center">
+              <CallLayout />
+              {/* {CallLayout()} */}
+            </div>
+            <div className={cn('h-[calc(100vh-86px)] hidden ml-2', {
+              'show-block': showParticipants
+            })}>
+              <CallParticipantsList onClose={()=> setShowParticipants(false)} />
+            </div>
         </div>
 
         <div className="fixed bottom-0 flex w-full items-center justify-center
@@ -80,8 +85,6 @@ const MeetingRoom = () => {
             {/* Button Not Working => CallParticipantsList not Working */}
             <button onClick={()=> {
               setShowParticipants((prev) => !prev)
-              console.log("button clicked");
-              
             }}>
               <div className="cursor-pointer rounded-2xl bg-[#19232d]
                 px-4 py-2 hover:bg-[#4c535b]">
